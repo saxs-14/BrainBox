a6687064f32bd4d9fb5b85c443c9e83d3d95e05c
Phathutshedzo Mamagau <mamagauphathutshedzosaxs@gmail.com>
2025-11-14 14:14:15 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } //
+        maven { url = uri("https://repository.liferay.com/nexus/content/repositories/public/") }
+    }
+}
+
+rootProject.name = "BrainBox"
+include(":app")
